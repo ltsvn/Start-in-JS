e8e934b4b53d5e435b02f954d788211b3b0d12dc
ltsvn <92997990+ltsvn@users.noreply.github.com>
2022-04-03 14:34:08 +0200

    create function that calculates body mass index

---
+++ Tasks/Task4/task4.js
@@ -0,0 +1,30 @@
+// Write function bmi that calculates body mass index (bmi = weight / height2).
+
+// if bmi <= 18.5 return "Underweight"
+
+// if bmi <= 25.0 return "Normal"
+
+// if bmi <= 30.0 return "Overweight"
+
+// if bmi > 30 return "Obese"
+
+
+function bmi(weight, height) {
+  
+    let bodyMaxIndex = weight / (height * height);
+    
+    if (bodyMaxIndex <= 18.5) { 
+      return "Underweight"; 
+    } else if (bodyMaxIndex <= 25.0) { 
+      return "Normal"; 
+      } else if (bodyMaxIndex <= 30.0) { 
+        return "Overweight"; 
+      } else if (bodyMaxIndex > 30) { 
+        return "Obese"; 
+      } else {
+        return "";
+      }
+  }
+
+
+  bmi(80, 1.80)
