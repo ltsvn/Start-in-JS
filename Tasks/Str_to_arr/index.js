5eee605d64908d1d806ad062c1098f958eb96438
ltsvn <92997990+ltsvn@users.noreply.github.com>
2022-04-03 14:41:59 +0200

    convert string to array of words

---
+++ Tasks/Str_to_arr/index.js
@@ -0,0 +1,7 @@
+// Write a function to split a string and convert it into an array of words.
+
+// Examples (Input -> Output):
+// * "Robin Singh" ==> ["Robin", "Singh"]
+
+
+const stringToArray = string => string.split(" ");
