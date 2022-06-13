5b44ccc7c7d020d3614a18aa3cf5206cfcf009c8
ltsvn <92997990+ltsvn@users.noreply.github.com>
2022-06-13 22:09:57 +0200

    array methods

---
+++ Lessons/Lesson 11.06.22/Arrays.js
@@ -0,0 +1,51 @@
+// const menuItems = ['Start', 'menu 1', 'menu 2', 'menu 3', 'menu 4']
+//
+// const list = document.getElementsByClassName('list');
+//
+//
+// console.log
+
+// const items = ["Start", "menu1", "menu2", "menu3", "menu4", "menu5"];
+// // // const newItems = new Array(1,2,3);
+// // //
+// // // console.log("Items: ", items);
+// // //
+// // // const lists = document.querySelectorAll('.list');
+// // // const list = lists[0];
+// // // const list2 = lists[1];
+// // //
+// // //
+// // // // for
+// // // for(let i = 0; i < items.length; i++){
+// // //     if(i < 3){
+// // //         const liE = document.createElement('li');
+// // //         liE.innerText = items[i];
+// // //         list.append(liE);
+// // //     }
+// // //     if(i >= 3 || i <= 5) {
+// // //         const liE = document.createElement('li');
+// // //         liE.innerText = items[i];
+// // //         list2.append(liE);
+// // //     }
+// // // }
+// // //
+// // // console.log("lIST:", list.classList);
+
+...................forEach...................
+
+// items.forEach((item, index) => {
+//     if (index < FIRST_LIST_LIMIT) {
+//         const liE = document.createElement('li');
+//         liE.innerText = item;
+//         list.append(liE);
+//     }
+//
+//     if (index >= FIRST_LIST_LIMIT && index <= SECOND_LIST_LIMIT) {
+//         const liE = document.createElement('li');
+//         liE.innerText = item;
+//         list2.append(liE);
+//     }
+// })
+
+............................
+
